baddbc3672fd82a6e4bed6a27852ee7eee550de5
Forest Lettuce <azureforestryan@gmail.com>
2019-03-27 16:14:58 +0000

    back-end stations

--- app/app.py
+++ app/app.py
@@ -1,9 +1,37 @@
-from flask import Flask
+from flask import Flask, g, jsonify
 from flask.templating import render_template
+import sqlalchemy as sqla
+from sqlalchemy import create_engine
+import json
+import sqlite3
+import pymysql
+pymysql.install_as_MySQLdb()
+
 app = Flask(__name__)
+#app.config.from_object('config')
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route("/stations")
+def get_stations():
+    # return a list of all stations
+    stations=[]
+    conn = get_db()
+    rows = conn.execute("SELECT * from stations;")
+    for row in rows:
+        stations.append(dict(row))
+    return jsonify(stations=stations)
+
+def get_db():
+    URI="dbikes.cojxe4plci1a.us-east-1.rds.amazonaws.com"
+    PORT="3306"
+    DB="dbikes"
+    USER="forest"
+    PASSWORD="66666666"
+    engine=create_engine("mysql+mysqldb://{}:{}@{}:{}/{}".format(USER,PASSWORD,URI,PORT,DB),echo=True)
+    return engine
+
 if __name__ == '__main__':
     app.run(debug=True)
--- app/static/js/map_weather.js
+++ app/static/js/map_weather.js
@@ -1,119 +1,153 @@
+var map;
+var infobox;
 function loadMapScenario() {
-    var map = new Microsoft.Maps.Map(document.getElementById('myMap'), {
+    map = new Microsoft.Maps.Map(document.getElementById('myMap'), {
         mapTypeId: Microsoft.Maps.MapTypeId.road,
         center: new Microsoft.Maps.Location(53.3498, -6.2603),
         zoom: 14
     });
 
-    var infobox = new Microsoft.Maps.Infobox(map.getCenter(), {
+    infobox = new Microsoft.Maps.Infobox(map.getCenter(), {
         visible: false,
         maxWidth: 350,
         maxHeight: 200,
-        description:'<div style="height:20px; overflow: auto;"><div>'
+        description: '<div style="height:20px; overflow: auto;"><div>'
     });
 
     infobox.setMap(map);
 
-    function a(obj) {
-        for (var i = 0; i < obj.length; i++) {
-            var number = obj[i].number;
-            var name = obj[i].name;
-            //console.log(obj[i].name)
-            var lat = obj[i].position.lat;
-            var lng = obj[i].position.lng;
-            var bike_stands = obj[i].bike_stands;
-            var available_bike_stands = obj[i].available_bike_stands;
-            var available_bikes = obj[i].available_bikes;
-            var address = obj[i].address;
-            var loc = new Microsoft.Maps.Location(lat, lng);
-            if (available_bike_stands != 0) {
-                var userPin = new Microsoft.Maps.Pushpin(map.getCenter(), {
-                    visible: false,
-                    color: Microsoft.Maps.Color.fromHex('#000D29'),
-                    roundClickableArea: true
-                });
-                userPin.setLocation(loc);
-                userPin.setOptions({
-                    visible: true
-                });
-                userPin.metadata = {
-                    title: 'Bike Station: ' + number,
-                    description: 'Station Name: ' + name + '<br/>Bike Stands: ' + bike_stands + '<br/>Available Bike Stands: ' + available_bike_stands + '<br/>Available Bikes: ' + available_bikes + '<br/>Address: ' + address,
-                    location0: loc,
-                    lat0: lat,
-                    lng0: lng
-                };
+
+
+    getStations();
+}
 
 
 
-                //map.entities.push(userPin);
-            } else {
-                var userPin = new Microsoft.Maps.Pushpin(map.getCenter(), {
-                    //icon: 'https://www.bingmapsportal.com/Content/images/poi_custom.png',
-                    visible: false,
-                    //text: number.toString(),
-                    color: Microsoft.Maps.Color.fromHex('#F14D49'),
-                    roundClickableArea: true
-                });
-                userPin.setLocation(loc);
-                userPin.setOptions({
-                    visible: true
-                });
-                userPin.metadata = {
-                    title: 'Bike Station: ' + number,
-                    description: 'Station Name: ' + name + '<br/>Bike Stands: ' + bike_stands + '<br/>Available Bike Stands: ' + available_bike_stands + '<br/>Available Bikes: ' + available_bikes + '<br/>Address: ' + address,
-                    location0: loc,
-                    lat0: lat,
-                    lng0: lng
-                };
-            };
-
-            Microsoft.Maps.Events.addHandler(userPin, 'click', pushpinClicked);
-            map.entities.push(userPin);
-
-        }
-        
-
-
-        function pushpinClicked(e) {
-            //Make sure the infobox has metadata to display.
-            if (e.target.metadata) {
-                //Set the infobox options with the metadata of the pushpin.
-                infobox.setOptions({
-                    location: e.target.metadata.location0,
-                    title: e.target.metadata.title,
-                    description: e.target.metadata.description,
-                    visible: true,
-                    actions: [{
-                            label: 'Dublin Weather',
-                            eventHandler: function () {
-                                var ifrm = document.getElementById('forecast_embed');
-                                ifrm.src = '//forecast.io/embed/#lat=' + e.target.metadata.lat0 + '&lon=' + e.target.metadata.lng0 + '&name=Dublin';
-                            }
-
-                                },
-                        {
-                            label: 'Biking Chart',
-                            eventHandler: function () {
-                                alert('Chart for Biking');
-                            }
-                                }
-                            ]
-                });
-            }
-        }
+/*var xmlhttp = new XMLHttpRequest();
+xmlhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+        var myObj = JSON.parse(this.responseText);
+        a(myObj)
 
     }
+};
+xmlhttp.open("GET", "/static/js/stations.json", true);
+xmlhttp.send();*/
 
-    var xmlhttp = new XMLHttpRequest();
-    xmlhttp.onreadystatechange = function () {
-        if (this.readyState == 4 && this.status == 200) {
-            var myObj = JSON.parse(this.responseText);
-            a(myObj)
 
+
+function pushpinClicked(e) {
+    //Make sure the infobox has metadata to display.
+    if (e.target.metadata) {
+        //Set the infobox options with the metadata of the pushpin.
+        infobox.setOptions({
+            location: e.target.metadata.location0,
+            title: e.target.metadata.title,
+            description: e.target.metadata.description,
+            visible: true,
+            actions: [{
+                    label: 'Dublin Weather',
+                    eventHandler: function () {
+                        var ifrm = document.getElementById('forecast_embed');
+                        ifrm.src = '//forecast.io/embed/#lat=' + e.target.metadata.lat0 + '&lon=' + e.target.metadata.lng0 + '&name=Dublin';
+                    }
+
+                                },
+                {
+                    label: 'Biking Chart',
+                    eventHandler: function () {
+                        alert('Chart for Biking');
+                    }
+                                }
+                            ]
+        });
+    }
+}
+
+function a(obj) {
+    for (var i = 0; i < obj.length; i++) {
+        var number = obj[i].number;
+        var name = obj[i].name;
+        //console.log(obj[i].name)
+        var lat = obj[i].lat;
+        var lng = obj[i].lng;
+        //var bike_stands = obj[i].bike_stands;
+        //var available_bike_stands = obj[i].available_bike_stands;
+        //var available_bikes = obj[i].available_bikes;
+        var address = obj[i].address;
+        var loc = new Microsoft.Maps.Location(lat, lng);
+        var userPin = new Microsoft.Maps.Pushpin(map.getCenter(), {
+            visible: false,
+            color: Microsoft.Maps.Color.fromHex('#000D29'),
+            roundClickableArea: true
+        });
+        userPin.setLocation(loc);
+        userPin.setOptions({
+            visible: true
+        });
+        userPin.metadata = {
+            title: 'Bike Station: ' + number,
+            description: 'Station Name: ' + name + '<br/>Address: ' + address,
+            location0: loc,
+            lat0: lat,
+            lng0: lng
+        };
+        /*if (available_bike_stands != 0) {
+                var userPin = new Microsoft.Maps.Pushpin(map.getCenter(), {
+                visible: false,
+                color: Microsoft.Maps.Color.fromHex('#000D29'),
+                roundClickableArea: true
+            });
+            userPin.setLocation(loc);
+            userPin.setOptions({
+                visible: true
+            });
+            userPin.metadata = {
+                title: 'Bike Station: ' + number,
+                description: 'Station Name: ' + name + '<br/>Bike Stands: ' + bike_stands + '<br/>Available Bike Stands: ' + available_bike_stands + '<br/>Available Bikes: ' + available_bikes + '<br/>Address: ' + address,
+                location0: loc,
+                lat0: lat,
+                lng0: lng
+            };
+
+
+
+            //map.entities.push(userPin);
+        } else {
+            var userPin = new Microsoft.Maps.Pushpin(map.getCenter(), {
+                //icon: 'https://www.bingmapsportal.com/Content/images/poi_custom.png',
+                visible: false,
+                //text: number.toString(),
+                color: Microsoft.Maps.Color.fromHex('#F14D49'),
+                roundClickableArea: true
+            });
+            userPin.setLocation(loc);
+            userPin.setOptions({
+                visible: true
+            });
+            userPin.metadata = {
+                title: 'Bike Station: ' + number,
+                description: 'Station Name: ' + name + '<br/>Bike Stands: ' + bike_stands + '<br/>Available Bike Stands: ' + available_bike_stands + '<br/>Available Bikes: ' + available_bikes + '<br/>Address: ' + address,
+                location0: loc,
+                lat0: lat,
+                lng0: lng
+            };
+        };*/
+
+        Microsoft.Maps.Events.addHandler(userPin, 'click', pushpinClicked);
+        map.entities.push(userPin);
+
+    }
+}
+
+function getStations() {
+    $.ajax({
+        url: "/stations",
+        type: "GET",
+        dataType: "json",
+        success: function (data) {
+            a(data["stations"])
         }
-    };
-    xmlhttp.open("GET", "/static/js/stations.json", true);
-    xmlhttp.send();
+    });
 
 }
